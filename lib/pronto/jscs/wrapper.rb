c160bb129c81af6ff2767756bc3d68c0a555e958
Tomas Varaneckas <tomas.varaneckas@gmail.com>
2015-12-05 10:20:00 +0200

    Use open3 instead of backticks and handle stderr

--- lib/pronto/jscs/wrapper.rb
+++ lib/pronto/jscs/wrapper.rb
@@ -1,3 +1,4 @@
+require 'open3'
 module Pronto
   module JSCS
     class Wrapper
@@ -10,8 +11,10 @@ module Pronto
         path = @patch.new_file_full_path.to_s
         params = '--reporter=json'
         params << ' -p airbnb' unless File.exist?('.jscsrc')
-        output = `jscs "#{path}" #{params}`
-        JSON.parse(output)
+        stdout, stderr, _status = Open3.capture3("jscs \"#{path}\" #{params}")
+        puts "WARN: pronto-jscs: #{stderr}" if stderr && stderr.size > 0
+        return [] if stdout.nil? || stdout.size == 0
+        JSON.parse(stdout)
       rescue => e
         puts "ERROR: pronto-jscs failed to process a diff: #{e}"
         []
